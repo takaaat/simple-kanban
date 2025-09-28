aa8b2d9eb129df449a014a39372eb53740e60175
takaaat <sasah0525@gmail.com>
2025-09-28 15:48:30 +0900

    feat: タスク名の編集機能

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@
 import { useState } from "react";
 import { CardBox } from "@/components/CardBox";
 import { TaskCard } from "@/components/TaskCard";
-import type { CardArea } from "../types/types";
+import type { CardArea, Task } from "../types/types";
 
 export default function Home() {
   const data: CardArea[] = [
@@ -15,17 +15,38 @@ export default function Home() {
         { id: 1, name: "taskname2" },
       ],
     },
-    { id: 1, name: "area2", tasks: [{ id: 0, name: "taskname3" }] },
+    { id: 1, name: "area2", tasks: [{ id: 2, name: "taskname3" }] },
   ];
  
   const [kanban, setKanban] = useState<CardArea[]>(data);
+  const [editing, setEditing] = useState<number>(-1);
 
   function addTask(area: CardArea, name: string) {
+    const count = data.reduce((prev, value) => {return prev + value.tasks.length;}, 0);
     setKanban(kanban.map((currentArea) => {
       if (currentArea.id === area.id) {
-        return {...currentArea, tasks: [...currentArea.tasks, {id: currentArea.tasks.length, name: name}]};
+        return {...currentArea, tasks: [...currentArea.tasks, {id: count+1, name: name}]};
       }
       return currentArea;
+    }));
+  }
+
+  function setEditingFocus(id: number) {
+    setEditing(id);
+  }
+
+  function unFocus() {
+    setEditing(-1);
+  }
+
+  function editTask(targetTask: Task, newName: string) {
+    setKanban(kanban.map((currentArea) => {
+      return {...currentArea, tasks: currentArea.tasks.map((task) => {
+        if (task === targetTask) {
+          return {...task, name: newName};
+        }
+        return task;
+      })};
     }));
   }
 
@@ -35,7 +56,7 @@ export default function Home() {
         return (
           <CardBox name={area.name} key={area.id} onAdd={() => addTask(area, "aaa")}>
             {area.tasks.map((task) => {
-              return <TaskCard key={task.id} name={task.name} />;
+              return <TaskCard key={task.id} task={task} editing={editing} onClick={() => setEditingFocus(task.id)} unFocus={unFocus} onChange={editTask} />;
             })}
           </CardBox>
         );
--- src/components/TaskCard.tsx
+++ src/components/TaskCard.tsx
@@ -1,6 +1,34 @@
+import { useEffect, useRef } from "react";
+import type { Task } from "@/types/types";
+
 type Props = {
-    name: string;
-}
-export function TaskCard({ name }: Props) {
-    return <div className="bg-white w-full text-lg border-2 p-2 mb-2 cursor-pointer">{name}</div>;
+  task: Task;
+  editing: number;
+  onClick: () => void;
+  onChange: (targetTask: Task, newName: string) => void;
+  unFocus: () => void;
+};
+
+export function TaskCard({ unFocus, task, editing, onClick, onChange }: Props) {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleBlur = () => {
+    unFocus();
+  }
+
+  useEffect(() => {
+    if (task.id === editing && inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, [editing, task.id]);
+
+  return (
+    <button type="button" onClick={onClick} className="bg-white w-full text-lg border-2 p-2 mb-2 cursor-pointer text-left">
+        {(task.id === editing) ? 
+          <input ref={inputRef} value={task.name} onChange={(e) => onChange(task, e.target.value)} className="w-full"  onBlur={handleBlur} />
+         : 
+        <div>{task.name}</div>
+         }
+    </button>
+  );
 }
